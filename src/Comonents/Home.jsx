cd8eba28ad7b4761f8403c5a5b834aa272f9ce1c
master172 <themasterprogrammer70000@gmail.com>
2025-09-11 20:52:06 +0530

    fix title

--- src/Comonents/Home.jsx
+++ src/Comonents/Home.jsx
@@ -1,12 +1,11 @@
 import "../Styles/Home.css";
 
 const Home = ({ setCurrentPage, theme }) => {
-    const titleGradient = theme === 'gold' ? 'linear-gradient(45deg, #facc15, #b45f06)' : 'linear-gradient(45deg, #ef4444, #7f1d1d)';
     const exploreButtonColor = theme === 'gold' ? '#facc15' : '#ef4444';
 
     return (
         <div className="main-content home-content">
-            <h2 className="title-text animate-fade-in-up" style={{ backgroundImage: titleGradient }}>
+            <h2 className="title-text animate-fade-in-up" >
                 EQUINOX
             </h2>
             <p className="subtitle-text animate-fade-in-up delay-200" style={{ color: exploreButtonColor }}>
